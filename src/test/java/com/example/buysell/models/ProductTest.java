10c2a929127efa529c7a36c0a2b1850090b7479e
DanilPomortsev <danul78969@gmail.com>
2023-12-07 16:29:19 +0300

    docker

--- src/test/java/com/example/buysell/models/ProductTest.java
+++ src/test/java/com/example/buysell/models/ProductTest.java
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
 package com.example.buysell.models;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ProductTest {
-
-=======
-package com.example.buysell.models;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ProductTest {
-
->>>>>>> 06bda36450f02745f5b39bba4977e51d74f3d0ce
 }
